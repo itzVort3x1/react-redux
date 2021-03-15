24ea4da65b72a93f2cfc075c8ef13fc6214d7c8f
Kaustubh <saikaustubh10@gmail.com>
2021-03-15 08:23:04 +0530

    added navigation and components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>PageOne
+        <Link to="/pagetwo">Navigate to Page two</Link>
+    </div>
+}
+
+const PageTwo = () => {
+    return <div>PageTwo
+        <button>Click me</button>
+        <Link to="/">Navigate to Page one</Link>
+    </div>
+}
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
